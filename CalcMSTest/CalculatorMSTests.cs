e5d581d782cf2093d7c8d728edd5a9fae1956b4b
Nikos S <niksal1991@gmail.com>
2022-03-12 17:09:01 +0200

    Calculator Add Numbers and MSUnit Testing

---
+++ CalcMSTest/CalculatorMSTests.cs
@@ -0,0 +1,22 @@
+﻿using Calc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcMSTest
+{
+    [TestClass]
+    public class CalculatorMSTests
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoIntegers_GetCorrectResult()
+        {
+            //Arrange phase
+            Calculator calculator = new Calculator();
+
+            //Act phase
+            int result = calculator.AddNumbers(10, 20);
+
+            //Assert phase
+            Assert.AreEqual(30, result);
+        }
+    }
+}
